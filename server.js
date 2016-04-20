71cad0e062e1243595d49b9c335958e13302e253
Magne Davidsen <magne.davidsen@gmail.com>
2016-04-20 15:14:50 +0200

    getting body variables

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var restify = require('restify');
 
 var server = restify.createServer();
+server.use(restify.bodyParser());
+
 server.post('/shorten', (req, res, next) => {
 
-    if(req.params.token == process.env.SLACK_TOKEN){
-        console.log(req.params.token);
+    if(req.body.token == process.env.SLACK_TOKEN){
+        console.log(req.body.token);
         console.log(process.env.SLACK_TOKEN);
+        res.send('nice');
     }
     else {
         res.send(401, {error: 'Incorrect token.'});
