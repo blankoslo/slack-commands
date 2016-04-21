022367e974a88f4833591b63afedcd5173ed44fa
Magne Davidsen <magne.davidsen@gmail.com>
2016-04-21 16:12:57 +0200

    cleaning up

--- server.js
+++ server.js
@@ -5,21 +5,6 @@ var server = restify.createServer();
 
 server.use(restify.urlEncodedBodyParser({ mapParams : false }));
 
-function shortswitch(url, alias){
-    fetch('http://api.shortswitch.com/shorten',
-        { method: 'POST', body: 'longUrl=' + url + '&apiKey=' + process.env.SHORTSWITCH_TOKEN })
-	   .then(function(res) {
-
-           console.log("jai");
-		    return res.json();
-
-
-	    }).then(function(json){
-            console.log("ai");
-            console.log(json);
-	    });
-}
-
 //stupid stupid shortswitch api
 function firstAttribute(obj) {
     for (var a in obj) return obj[a];
@@ -28,21 +13,25 @@ function firstAttribute(obj) {
 server.post('/shorten', (req, res) => {
     if(req.body.token == process.env.SLACK_TOKEN){
         var textArray = req.body.text.split(' ');
+        var reqBody = 'apiKey=' + process.env.SHORTSWITCH_TOKEN +' +&longUrl=' + textArray[0];
+        if(textArray.length > 1) reqBody += '&token=' + textArray[1];
         fetch('http://api.shortswitch.com/shorten',
-            { method: 'POST', headers: {'Accept':'application/json'}, body: 'apiKey=' + process.env.SHORTSWITCH_TOKEN +' +&longUrl=' + textArray[0] +' +&token=' + textArray[1]})
+            { method: 'POST', headers: {'Accept':'application/json'}, body: reqBody})
     	   .then((shortswitchRes) => {
                shortswitchRes.json().then((jsonnn) => {
                    var shortBody = firstAttribute(jsonnn.results)
                    res.send({ 'response_type': 'in_channel', 'text': 'Ah yiss, du har kort URL! ('+shortBody.shortUrl+')',
                    'attachments': [{'text' : 'Den lange: ' + shortBody.longUrl} ] });
                })
+           }).catch((ex) => {
+               console.error('Error: ', ex)
+               res.send(500, {error: 'From shortswitch: ' + ex});
            });
        }
     else {
         res.send(401, {error: 'Incorrect token.'});
     }
 });
-
 
 server.listen(process.env.PORT || 3000, () => {
   console.log('%s listening at %s', server.name, server.url);
